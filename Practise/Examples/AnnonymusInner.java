f6bfed133edfed1db957d7a69e29bd2817bb332f
Gunasekar123 <47343570+Gunasekar123@users.noreply.github.com>
2019-02-20 09:54:28 +0530

    Create AnnonymusInner.java

---
+++ Practise/Examples/AnnonymusInner.java
@@ -0,0 +1,31 @@
+public class AnnonymusInner extends Thread{
+     public static void main(String []args){
+        AnnonymusInner h=new AnnonymusInner();
+       Thread t1=new Thread(h){
+           public void run(){
+             System.out.println("This is t1 thread"); 
+             h.multiply(5);
+           }
+       };
+         Thread t2=new Thread(h){
+           public void run(){
+             System.out.println("This is t2 thread");  
+             h.multiply(6);
+           }
+       };
+         Thread t3=new Thread(h){
+           public void run(){
+             System.out.println("This is t3 thread");  
+             h.multiply(7);
+           }
+       };
+       t1.start();
+       t2.start();
+       t3.start();
+     }
+     synchronized void multiply(int num){
+         for(int i=1;i<=10;i++){
+             System.out.println(num*i);
+         }
+     }
+}
