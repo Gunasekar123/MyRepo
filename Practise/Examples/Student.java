b9f114e0c5380b0d44e734bfe1635420b4766176
Gunasekar123 <47343570+Gunasekar123@users.noreply.github.com>
2019-02-28 11:53:50 +0530

    Create Student.java

---
+++ Practise/Examples/Student.java
@@ -0,0 +1,33 @@
+
+//This is an example for comparable interface
+import java.util.*;
+import java.io.*;
+class Student implements Comparable<Student>{
+int rollno;
+String name;
+int age;
+Student(int rollno,String name,int age){
+this.rollno=rollno;
+this.name=name;
+this.age=age;
+}
+
+public int compareTo(Student st){
+
+return this.age-st.age;
+}
+}
+
+public class TestSort3{
+public static void main(String args[]){
+ArrayList<Student> al=new ArrayList<Student>();
+al.add(new Student(101,"Vijay",23));
+al.add(new Student(106,"Ajay",27));
+al.add(new Student(105,"Jai",21));
+
+Collections.sort(al);
+for(Student st:al){
+System.out.println(st.rollno+" and1 "+st.name+" and "+st.age);
+  }
+}
+}
